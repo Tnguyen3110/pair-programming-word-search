1b62a18842771d7791466a89b9795aa65067d8d3
Tra Nguyen <ntptra85@gmail.com>
2023-05-01 14:33:30 -0600

    Initial commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,13 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = letters[0].map((col, i) => letters.map(row => row[i]).join(''));
+  const allStrings = horizontalJoin.concat(verticalJoin);
+  for (let string of allStrings) {
+    if (string.includes(word)) {
+      return true;
     }
+  }
+  return false;
 }
 
 module.exports = wordSearch
